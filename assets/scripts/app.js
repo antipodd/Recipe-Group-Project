d2c2a70434871220ab7bfe6b65f119463ef0c86c
Karel Hartlieb <Karel Hartlieb>
2017-03-19 15:07:14 -0500

    display 10 results and added checkboxes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -62,16 +62,40 @@ $('#submit-search').on('click', function(event){
 	  throw err;
 	});
 
+	$(".results-section").removeClass("results-section");
+
 });
 
 	function loadHTML (response) {
-		var imageURL = response.hits[0].recipe.image;
-		var title = response.hits[0].recipe.label;
-		var returnURL = response.hits[0].recipe.url;
-		console.log (imageURL + title + returnURL);
-		$(".results").append("<img class ='recipePhoto' src='" + imageURL + "'/>");
-		$(".results").append("<h2>" + title + "</h2>");
-		// $(".results").append("<h2>" + title + "</h2>");
+		for (var i = 0; i < response.hits.length; i++) {
+			if (i === 0 || i%3 === 0) {
+				var newRow = $("<div class = row>");
+				var newColumn = $("<div>");
+				newColumn.addClass("col-xs-12 col-sm-6 col-md-4 col-lg-4")
+				var imageURL = response.hits[i].recipe.image;
+				var title = response.hits[i].recipe.label;
+				var returnURL = response.hits[i].recipe.url;
+				newColumn.append("<a href='" + returnURL + "''>" + "<img class ='recipePhoto' src='" + imageURL + "'/>" + "<h2>" + title + "</h2>" + "</a>" );
+				var newCheckbox = $("<div>");
+				newCheckbox.addClass("checkbox");
+				newCheckbox.append("<label><input type=\"checkbox\" id=\"recipe-result\" value=" + response.hits[i].recipe.uri + "><span>Save Recipe</span></label>");
+				newColumn.append(newCheckbox);
+				newRow.append(newColumn);
+				$(".results").append(newRow);
+			} else {
+				var newColumn = $("<div>");
+				newColumn.addClass("col-xs-12 col-sm-6 col-md-4 col-lg-4")
+				var imageURL = response.hits[i].recipe.image;
+				var title = response.hits[i].recipe.label;
+				var returnURL = response.hits[i].recipe.url;
+				newColumn.append("<a href='" + returnURL + "''>" + "<img class ='recipePhoto' src='" + imageURL + "'/>" + "<h2>" + title + "</h2>" + "</a>" );
+				var newCheckbox = $("<div>");
+				newCheckbox.addClass("checkbox recipe-result");
+				newCheckbox.append("<label><input type=\"checkbox\" id=\"recipe-result\" value=" + response.hits[i].recipe.uri + "><span>Save Recipe</span></label>");
+				newColumn.append(newCheckbox);
+				$(".results .row:last-child").append(newColumn);
+			}
+		}
 	}
 	
 
@@ -84,8 +108,20 @@ function pullCheckboxValues(array, formgroup){
 		});
 	});
 }
-
-
+//add text change upon click and also save the response.hits[i].recipe.uri to firebase - can use uri for ajax request for saved recipes
+$(".results").on("click", "#recipe-result", function() {
+	var value = $(this).val();
+	console.log(value)
+	var checked = this.checked;
+	console.log(checked);
+	if (checked) {
+		$(this).siblings("span").html("Recipe Saved!");
+		//add value to firebase
+  } else {
+    $(this).siblings("span").html("Save Recipe");
+    //remove value from firebase
+  }
+});
 
   // Initialize Firebase
   
