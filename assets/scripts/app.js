9737a5954256ee3cf67fd585fbad340f4d0e7800
Taniesha Robinson <taniesharobinson@mcs-macbook06.att.net>
2017-03-22 07:48:51 -0500

    added restaurant page and form validation for empty submission

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,52 +24,57 @@ var userState = {
 }
 
 $('#submit-search').on('click', function(event){
-	$(".results").empty();
-	counter = 0;
 	
 	event.preventDefault();
-	console.log("test");
+	$(".results").empty();
+	counter = 0;
 
-	userState.ingredients = $('#ingredients-input').val().trim();
-	userState.allergies = [];
-	userState.dietPrefs = [];
-	userState.time = $('.time-input').val();
-	pullCheckboxValues(userState.allergies,".allergyList");
-	pullCheckboxValues(userState.dietPrefs,".dietPrefs");
-	
-	console.log("allergies " + userState.allergies, "dietPrefs " + userState.dietPrefs);
-	console.log("ingredients " + userState.ingredients);
-	console.log("time " + userState.time);
+	if( $('#ingredients-input').val() === ""){
+		$(this).after("<p class=\"invalid-input\">Please enter a valid search value</p>");
+	} else {
+		$('.invalid-input').hide();
+		userState.ingredients = $('#ingredients-input').val().trim();
+		userState.allergies = [];
+		userState.dietPrefs = [];
+		userState.time = $('.time-input').val();
+		pullCheckboxValues(userState.allergies,".allergyList");
+		pullCheckboxValues(userState.dietPrefs,".dietPrefs");
+		
+		console.log("allergies " + userState.allergies, "dietPrefs " + userState.dietPrefs);
+		console.log("ingredients " + userState.ingredients);
+		console.log("time " + userState.time);
 
-	var allergiesQuery = "";
-	var dietPrefsQuery = "";
+		var allergiesQuery = "";
+		var dietPrefsQuery = "";
 
-	for(i = 0; i < userState.allergies.length; i++){
-		allergiesQuery += "&health=" + userState.allergies[i]
-		console.log(allergiesQuery);
+		for(i = 0; i < userState.allergies.length; i++){
+			allergiesQuery += "&health=" + userState.allergies[i]
+			console.log(allergiesQuery);
+		}
+
+		for(i = 0; i < userState.dietPrefs.length; i++){
+			dietPrefsQuery += "&health=" + userState.dietPrefs[i]
+			console.log(dietPrefsQuery);
+		}
+
+
+		var url = "https://api.edamam.com/search?q=" + userState.ingredients + "&app_idbcb68bd8" + "&app_key=2a8d5e5d4600120a11ab487124231f6c" + "&from=" + userState.from + "&to=" + userState.to + dietPrefsQuery + allergiesQuery;
+		console.log(url);
+		
+
+		$.ajax({
+		  url: url,
+		  method: 'GET',
+		}).done(function(response) {
+		  console.log(response);
+		  loadHTML(response);
+		}).fail(function(err) {
+		  throw err;
+		});
+
+		$(".results-section").removeClass("results-section");
+
 	}
-
-	for(i = 0; i < userState.dietPrefs.length; i++){
-		dietPrefsQuery += "&health=" + userState.dietPrefs[i]
-		console.log(dietPrefsQuery);
-	}
-
-
-	var url = "https://api.edamam.com/search?q=" + userState.ingredients + "&app_idbcb68bd8" + "&app_key=2a8d5e5d4600120a11ab487124231f6c" + "&from=" + userState.from + "&to=" + userState.to + dietPrefsQuery + allergiesQuery;
-	console.log(url);
-	
-
-	$.ajax({
-	  url: url,
-	  method: 'GET',
-	}).done(function(response) {
-	  console.log(response);
-	  loadHTML(response);
-	}).fail(function(err) {
-	  throw err;
-	});
-
-	$(".results-section").removeClass("results-section");
 
 });
 
@@ -113,6 +118,7 @@ $('#submit-search').on('click', function(event){
 		newLoadColumn.append(moreResultsButton);
 		newLoadRow.append(newLoadColumn);
 		$(".results").append(newLoadRow);
+		$(".end-message").show();
 	}
 	
 
@@ -172,7 +178,6 @@ $(".results").on('click', "#load-more-results", function(){
 
 });
 
-  // Initialize Firebase
   
 
    
